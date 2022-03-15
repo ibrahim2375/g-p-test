22defe52249015abdddb7b64d510b7a08ef38c90
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-03-15 23:59:00 +0200

    kk

--- src/routes/web/api.js
+++ src/routes/web/api.js
@@ -5,14 +5,8 @@ const session = require('express-session');
 const { sequelize } = require('sequelize');
 
 router.get('/', async (req, res) => {
-    // const dataOfInstructor = await db.users.findOne({ where: { email: req.session.user.email }, include: [db.instructor] });
-    // var studentResult = await db.csisResult.findAll({ where: { courseName: dataOfInstructor.instructors[0].courseName } });
-    // const studentResult = await db.csisResult.findAll({ where: { userId: 20210000, courseName: 'math1' } });
-    // const sonResult = await db.csisResult.findAll({ where: { userId: 20210000 }, include: db.users });
-    // const sonData = await db.csisStudent.findOne({ where: { userId: 20210000 }, include: db.users });
 
-    // getstudents.instructors[0].courseName
-    // res.send(sonData)
+    res.send('Hello in Api Page');
 
 });
 
